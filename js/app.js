ce75ab0ee0d88219adef4e556003824dcf001b69
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2024-01-31 19:01:31 +0330

    add logic calculator

--- js/app.js
+++ js/app.js
@@ -1,1 +1,30 @@
+let display = document.querySelector('.calculator_display')
+
+let equation = '';
+
+function clickNumber(number) {
+    equation += number;
+    updateDisplay();
+}
+function clickOperator(operator) {
+    if (equation.slice(-1).match(/[+\-*/.]/)) return;
+    equation += operator;
+    updateDisplay();
+}
+function updateDisplay() {
+    display.textContent = equation;
+}
+function clearDisplay() {
+    equation = '';
+    updateDisplay();
+}
+
+function clickResult() {
+    try {
+        equation = new Function('return ' + equation)();
+    } catch (error) {
+        equation = 'Error';
+    }
+    updateDisplay();
+}
 
